03f2797a043ae19bdb5d273d56413a500ccf827e
Gustav Liffner <gurra_liffner@hotmail.com>
2025-01-23 23:18:14 +0000

    Update app.js

--- 02week01lesson/app.js
+++ 02week01lesson/app.js
@@ -2,29 +2,29 @@
 var count = 0;
 // Update the count display
 var updateCountDisplay = function () {
-    var countElement = document.getElementById("count");
-    if (countElement !== null) {
-        countElement.textContent = count.toString();
-    }
+  var countElement = document.getElementById("count");
+  if (countElement !== null) {
+    countElement.textContent = count.toString();
+  }
 };
 // Increment the count
 var increment = function () {
-    count += 1;
-    updateCountDisplay();
+  count += 1;
+  updateCountDisplay();
 };
 // Decrement the count
 var decrement = function () {
-    count -= 1;
-    updateCountDisplay();
+  count -= 1;
+  updateCountDisplay();
 };
 // Reset the count
 var reset = function () {
-    count = 0;
-    updateCountDisplay();
+  count = 0;
+  updateCountDisplay();
 };
 // Attach event listeners to buttons
-document.getElementById("increment-button").onclick =
-    increment;
-document.getElementById("decrement-button").onclick =
-    decrement;
+document.getElementById("increment-button").onclick = increment;
+document.getElementById("decrement-button").onclick = decrement;
 document.getElementById("reset-button").onclick = reset;
+
+console.log("hello");
